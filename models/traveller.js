ae7027fe5fad546ba28e83cc36f5339dcce08515
jamiebell941 <jamiebell941@gmail.com>
2020-03-25 17:45:04 +0000

    finished hw

---
+++ models/traveller.js
@@ -0,0 +1,38 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation);
+};
+
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  methodsOfTransport = this.journeys.map(journey => journey.transport);
+  return methodsOfTransport.filter((transport, train) => methodsOfTransport.indexOf(transport) === train );
+};
+//ngl I dunno how this one works, was just embarassed about asking it to explained for the 4th time
+
+module.exports = Traveller;
